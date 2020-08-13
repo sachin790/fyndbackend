b0e653b89e2549d153501eb999774393ea21229a
kishlayuaap@gmail.com <bhardwaj.sachin44@gmail.com>
2020-08-13 15:34:01 +0530

    --

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ app.use(function(err, req, res, next) {
 });
 
 //server 
-http.createServer(app).listen(3000, function()  {
+http.createServer(app).listen(process.env.PORT || 3000, function()  {
     console.log('Express server listening on port.. ' +3000);
   });
   
